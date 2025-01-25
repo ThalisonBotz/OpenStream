32999f83ef6f43a315cf9afb27e2abd3cc95c7a4
Thalison Botelho <thalison.botelho.silva@gmail.com>
2025-01-25 14:18:17 -0300

    add category model, migration, and AdminJS resource integration

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -2,11 +2,13 @@ import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { database } from '../database'; // Importing the database
+import { adminJsResources } from "./resources";
 
 AdminJS.registerAdapter(AdminJSSequelize); // Registering the adapter for Sequelize
 
 export const adminJs = new AdminJS({
   databases: [database], // Registering the database
+  resources: adminJsResources, // Registering the resources
   rootPath: "/admin",  // The path where the admin panel will be available
   branding: {
     companyName: "OpenStream",
@@ -14,21 +16,21 @@ export const adminJs = new AdminJS({
     theme: {
       colors: {
         primary100: '#ff0043',
-	      primary80: '#ff1a57',
-	      primary60: '#ff3369',
-	      primary40: '#ff4d7c',
-		    primary20: '#ff668f',
-	      grey100: '#151515',
-	      grey80: '#333333',
-	      grey60: '#4d4d4d',
-	      grey40: '#666666',
-	      grey20: '#dddddd',
-	      filterBg: '#333333',
-	      accent: '#151515',
-	      hoverBg: '#151515',
+        primary80: '#ff1a57',
+        primary60: '#ff3369',
+        primary40: '#ff4d7c',
+        primary20: '#ff668f',
+        grey100: '#151515',
+        grey80: '#333333',
+        grey60: '#4d4d4d',
+        grey40: '#666666',
+        grey20: '#dddddd',
+        filterBg: '#333333',
+        accent: '#151515',
+        hoverBg: '#151515',
       }
     }
   }
-})
+});
 
 export const adminJsRouter = AdminJSExpress.buildRouter(adminJs); // Building the router
